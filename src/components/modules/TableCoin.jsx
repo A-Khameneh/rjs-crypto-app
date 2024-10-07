ee776383cd7acfa21e8a9361e206f71ea2b90887
a-khameneh <mt.d.abdollahian@gmail.com>
2024-10-07 22:01:08 +0330

    table coin styles

--- src/components/modules/TableCoin.jsx
+++ src/components/modules/TableCoin.jsx
@@ -1,28 +1,32 @@
+import { RotatingLines } from "react-loader-spinner";
+
 import chartUp from "../../assets/chart-up.svg";
 import chartDown from "../../assets/chart-down.svg";
 
+import styles from "./TableCoin.module.css";
+
 function TableCoin( {coins, isLoading} ) {
 
-    return (<div>
+    return (<div className={styles.container}>
 
-                {isLoading ? <p>Loading</p> : 
+                {isLoading ? <RotatingLines strokeColor="#3874ff" strokeWidth="2" /> : 
                 
-                    (<table>
+                    (<table className={styles.table}>
 
-                    <thead>
-                        <tr>
-                            <th>Coin</th>
-                            <th>Name</th>
-                            <th>Price</th>
-                            <th>24h</th>
-                            <th>Total Volume</th>
-                            <th></th>
-                        </tr>
-                    </thead>
+                        <thead>
+                            <tr>
+                                <th>Coin</th>
+                                <th>Name</th>
+                                <th>Price</th>
+                                <th>24h</th>
+                                <th>Total Volume</th>
+                                <th></th>
+                            </tr>
+                        </thead>
         
-                    <tbody>
-                        {coins.map ( coin => <TableRow key={coin.id} coin={coin} /> )}
-                    </tbody>
+                        <tbody>
+                            {coins.map ( coin => <TableRow key={coin.id} coin={coin} /> )}
+                        </tbody>
                 
                     </table>)
                 }
@@ -40,7 +44,7 @@ function TableRow({coin}) {
         <tr> 
 
             <td>
-                <div>
+                <div className={styles.symbol}>
                     <img src={coin.image} alt="" />
                     <span> {coin.symbol.toUpperCase()} </span>
                 </div>
@@ -48,7 +52,7 @@ function TableRow({coin}) {
 
             <td>{coin.name}</td>
             <td>${coin.current_price.toLocaleString()}</td>
-            <td>{coin.price_change_percentage_24h.toFixed(2)}%</td>
+            <td className={coin.price_change_percentage_24h > 0 ? styles.success : styles.err}>{coin.price_change_percentage_24h.toFixed(2)}%</td>
             <td>{coin.total_volume.toLocaleString()}</td>
             <td><img src={(coin.price_change_percentage_24h>0) ? chartUp : chartDown} alt={coin.name} /></td>
 
