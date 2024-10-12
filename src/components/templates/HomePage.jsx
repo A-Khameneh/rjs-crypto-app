c5b5cf47a6dbcd3a575bf644bdb25ecb4f6186ca
a-khameneh <mt.d.abdollahian@gmail.com>
2024-10-12 20:31:00 +0330

    use effect - cleanup function

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -3,12 +3,14 @@ import { useEffect, useState } from "react";
 import TableCoin from "../modules/TableCoin";
 import { getCoinList } from "../../services/cryptoApi";
 import Pagination from "../modules/Pagination";
+import Search from "../modules/Search";
 
 function HomePage() {
 
     const [coins, setCoins] = useState([]);
     const [isLoding, setIsLoading] = useState(true);
     const [page, setPage] = useState(1);
+    const [currency, setCurrency] = useState("usd");
 
     useEffect( () => {
 
@@ -21,23 +23,32 @@ function HomePage() {
 
         const getData = async () => {
 
-            setIsLoading(true);
-            const res = await fetch( getCoinList( page ), options );
-            const json = await res.json();
-            setCoins( json );
-            console.log(json);
-            setIsLoading( false );
+            try {
+
+                setIsLoading(true);
+                const res = await fetch( getCoinList( page, currency ), options );
+                const json = await res.json();
+                setCoins( json );
+                console.log(json);
+                setIsLoading( false );
+
+            } catch (err) {
+
+                console.log(err);
+
+            }
 
         }
 
         getData();
 
-    }, [page] );
+    }, [page, currency] );
 
     return (
 
         <div> 
-            
+
+            <Search currency={currency} setCurrency={setCurrency} />
             <TableCoin coins={ coins } isLoading={ isLoding } /> 
             <Pagination page={page} setPage={setPage} />
         
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,10 +1,8 @@
 const BASE_URL = "https://api.coingecko.com/api/v3";
 const API_KEY = "CG-XNoZsUzmC8REw3E2piQXv7V1";
 
-const getCoinList = ( page ) => {
+const getCoinList = ( page, currency ) => `${BASE_URL}/coins/markets?vs_currency=${currency}&per_page=20&page=${page}&x_cg_demo_api_key=${API_KEY}`
 
-    return `${BASE_URL}/coins/markets?vs_currency=usd&per_page=20&page=${page}&x_cg_demo_api_key=${API_KEY}`
+const searchCoin = (query) => `${BASE_URL}/search?query=${query}&x_cg_demo_api_key=${API_KEY}`;
 
-}
-
-export { getCoinList };
+export { getCoinList, searchCoin };
